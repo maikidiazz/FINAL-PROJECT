4f1c07e93037802af0357d8791cf1de78a7456b7
christian7880 <christian_3142250057@utd.edu.mx>
2026-06-16 09:34:16 -0600

    Add files via upload
    We upload the data base from our project

---
+++ Code/base _de _datos.py
@@ -0,0 +1,24 @@
+
+import mysql.connector
+from mysql.connector import Error
+
+
+def create_connection():
+    """Establece una conexión a la base de datos MySQL."""
+    conection = None
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            database="proyecto_final",
+            user="root",
+            password=""
+        )
+        if connection.is_connected():
+            print("Conexión exitosa a la base de datos")
+            return connection
+    except Error as e:
+        print(f"Error al conectar a la base de datos: {e}")
+        return None
+
+
+create_connection()
---
+++ Code/proyecto_final.sql
@@ -0,0 +1,87 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 16-06-2026 a las 17:32:34
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proyecto_final`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumnos`
+--
+
+CREATE TABLE `alumnos` (
+  `id_alumno` int(11) NOT NULL,
+  `Nombre_Alumno` varchar(50) NOT NULL,
+  `Matricula` int(20) NOT NULL,
+  `Camion` varchar(10) NOT NULL,
+  `Parada` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `autobuses`
+--
+
+CREATE TABLE `autobuses` (
+  `ID_Ruta` int(50) NOT NULL,
+  `Camion` varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `Hora_Salida` time NOT NULL,
+  `Asientos` int(50) NOT NULL,
+  `Estatus` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  ADD PRIMARY KEY (`id_alumno`);
+
+--
+-- Indices de la tabla `autobuses`
+--
+ALTER TABLE `autobuses`
+  ADD PRIMARY KEY (`ID_Ruta`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  MODIFY `id_alumno` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `autobuses`
+--
+ALTER TABLE `autobuses`
+  MODIFY `ID_Ruta` int(50) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
